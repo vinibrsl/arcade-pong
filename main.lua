c57adea98c38961abfff1b8a92a8c7fddad393b0
Vinicius Brasil <vnbrs@icloud.com>
2019-04-12 16:46:04 -0300

    Change player logic to class

--- main.lua
+++ main.lua
@@ -6,9 +6,45 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
-PADDLE_SPEED = 200
+BOUNDS_MIN_Y = 0
+BOUNDS_MAX_Y = (VIRTUAL_HEIGHT - 20)
+BOUNDS_MIN_X = 0
+BOUNDS_MAX_X = (VIRTUAL_WIDTH)
+
+local Ball = {}
+function Ball:new()
+    o = {
+        x = VIRTUAL_WIDTH / 2 - 2,
+        y = VIRTUAL_HEIGHT / 2 - 2,
+        x_velocity = (math.random(2) == 1 and 100 or -100),
+        y_velocity = math.random(-50, 50)
+    }
+
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+local Player = {}
+function Player:new(o)
+    o = { score = 0, y = 0, speed = 200 }
+
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function Player:move(direction, delta_time)
+    if direction == 'up' then
+        self.y = math.max(BOUNDS_MIN_Y, self.y + -self.speed * delta_time)
+    elseif direction == 'down' then
+        self.y = math.min(BOUNDS_MAX_Y, self.y + self.speed * delta_time)
+    end
+end
 
 function love.load()
+    math.randomseed(os.time())
+
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     love.graphics.setFont(
@@ -21,29 +57,42 @@ function love.load()
         vsync = true
     })
 
-    player_1 = {
-        score = 0,
-        y = 30
-    }
+    player_1 = Player:new()
+    player_1.y = 30
 
-    player_2 = {
-        score = 0,
-        y = VIRTUAL_HEIGHT - 50
-    }
+    player_2 = Player:new()
+    player_2.y = (VIRTUAL_HEIGHT - 50)
+
+    ball = Ball:new()
 end
 
 function love.update(delta_time)
     if love.keyboard.isDown('w') then
-        player_1.y = player_1.y + -PADDLE_SPEED * delta_time
+        player_1:move('up', delta_time)
     elseif love.keyboard.isDown('s') then
-        player_1.y = player_1.y + PADDLE_SPEED * delta_time
+        player_1:move('down', delta_time)
     end
 
     if love.keyboard.isDown('up') then
-        player_2.y = player_2.y + -PADDLE_SPEED * delta_time
+        player_2:move('up', delta_time)
     elseif love.keyboard.isDown('down') then
-        player_2.y = player_2.y + PADDLE_SPEED * delta_time
+        player_2:move('down', delta_time)
     end
+
+    if ball.y >= BOUNDS_MAX_Y or ball.y <= BOUNDS_MIN_Y then
+        ball.y_velocity = ball.y_velocity * (-1)
+    end
+
+    if ball.x >= BOUNDS_MAX_X then
+        player_1.score = player_1.score + 1
+        ball = Ball:new()
+    elseif ball.x <= BOUNDS_MIN_X then
+        player_2.score = player_2.score + 1
+        ball = Ball:new()
+    end
+
+    ball.x = ball.x + ball.x_velocity * delta_time
+    ball.y = ball.y + ball.y_velocity * delta_time
 end
 
 function love.keypressed(key)
@@ -55,12 +104,12 @@ end
 function love.draw()
     push:apply('start')
 
-    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
     drawScores()
     drawPaddles()
 
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle('fill', ball.x, ball.y, 4, 4)
 
     push:apply('end')
 end

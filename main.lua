c209cbc5940d2a0d45b91e5bbc84028baadfd010
Vinicius Brasil <vnbrs@icloud.com>
2019-04-12 09:43:31 -0300

    Create a virtual viewport with push.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
+push = require './vendor/push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -10,11 +17,9 @@ function love.load()
 end
 
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong',
-        0,
-        WINDOW_HEIGHT / 2 - 6,
-        WINDOW_WIDTH,
-        'center'
-    )
+    push:apply('start')
+
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    push:apply('end')
 end

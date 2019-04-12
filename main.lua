ddcd95d2c7497e905f2554f614ce7495fec3233b
Vinicius Brasil <vnbrs@icloud.com>
2019-04-12 09:44:48 -0300

    Add keyboard handling

--- main.lua
+++ main.lua
@@ -16,6 +16,12 @@ function love.load()
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
     push:apply('start')
 
